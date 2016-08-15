f53966e66ad5ca104035a105eb0eb1a1a4441531
sk9564021 <sk9564021@gmail.com>
2016-08-15 12:51:56 +0530

    Add files via upload

---
+++ ss/template_header.php
@@ -0,0 +1,17 @@
+<div id="pageHeader"><table width="100%" border="0" cellspacing="0" cellpadding="12">
+  <tr>
+    <td width="32%"><a href="http://www.classx.tk"><img src="http://inspirationfeed.com/wp-content/uploads/2010/09/31dc2b76d17c12fc6c02e255359267e11.png" alt="Logo" width="252" height="36" border="0" /></a></td>
+    <td width="68%" align="right"><a href="http://www.classx.tk/cart.php">Your Cart</a></td>
+  </tr>
+  <tr>
+    <td colspan="2"><a href="http://www.classx.tk/index.php">Home</a> &nbsp; &middot; &nbsp; <a href="storeadmin/index.php">Products</a> &nbsp; &middot; &nbsp; <a href="#">Help</a> &nbsp; &middot; &nbsp; <a href="#">Contact us</a></td>
+    </tr>
+  </table>
+</div>
+
+
+
+
+
+
+
